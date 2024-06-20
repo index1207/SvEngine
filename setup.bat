7d3d12857606992f48dfe0fdd3c01900d8381aaf
index <han93851207@gmail.com>
2024-06-20 13:30:34 +0900

    Remove copy.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,9 @@
+mkdir lib/bin
+cd lib/netcpp
+cmake . && cmake --build . --target netcpp --config Release
+cd ../oneTBB
+cmake . && cmake --build . --target tbb --config Release
+
 for /r .\lib\oneTBB\ %%i in (*.lib, *.dll) do (
     copy "%%i" "lib\bin"
 )
