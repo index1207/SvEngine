6db65ade1b04696747bc91df0c2d77d42fe71c94
index <han93851207@gmail.com>
2024-06-20 13:40:32 +0900

    Fix setup.bat

--- setup.bat
+++ setup.bat
@@ -1,9 +1,12 @@
+rem build library
 mkdir lib/bin
 cd lib/netcpp
 cmake . && cmake --build . --target netcpp --config Release
 cd ../oneTBB
 cmake . && cmake --build . --target tbb --config Release
+cd ../../
 
+rem copy library files
 for /r .\lib\oneTBB\ %%i in (*.lib, *.dll) do (
     copy "%%i" "lib\bin"
 )
