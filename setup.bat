af769e8149d58ba506a58f1be5eaa5ebf5a34529
index <han93851207@gmail.com>
2024-06-20 14:58:56 +0900

    Debug with Release build

--- setup.bat
+++ setup.bat
@@ -1,16 +1,25 @@
-rem build library
-mkdir .\lib\bin
-cd lib\netcpp
-cmake . && cmake --build . --target netcpp --config Release
-cd ..\oneTBB
-cmake . && cmake --build . --target tbb --config Release
-cd ..\..\
+@echo off
 
-rem copy library files
+mkdir .\lib\bin\Debug
+mkdir .\lib\bin\Release
+
+call :build Debug
+call :build Release
+EXIT /B %ERRORLEVEL%
+
+:build
+cd lib\netcpp   
+cmake . && cmake --build . --target netcpp --config %~1
+cd ..\oneTBB
+cmake . && cmake --build . --target tbb --config %~1
+cd ..\..\
 for /r .\lib\oneTBB\ %%i in (*.lib, *.dll) do (
-    copy "%%i" "lib\bin"
+    copy "%%i" "lib\bin\%~1\"
+    del "%%i"
 )
 
 for /r .\lib\netcpp\ %%i in (*.lib) do (
-    copy "%%i" "lib\bin"
+    copy "%%i" "lib\bin\%~1\"
+    del "%%i"
 )
+EXIT /B 0
